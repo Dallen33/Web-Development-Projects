1ecffbcd4333234f7ee0d4d0fcc2ebdb302e5035
Dallen33 <157931457+Dallen33@users.noreply.github.com>
2024-04-14 23:28:52 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
+import express, { urlencoded } from 'express';
+const app = express();
+const port = 3000;
+
+app.use(urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/contact.html'); // send HTML file on GET request
+});
+
+app.post('/submit-form', (req, res) => {
+    const username = req.body.username; // access form data
+    // Add validation logic here
+    res.send('Username is $dhamil51');
+});
+
+app.listen(port, () => {
+    console.log('Server running on http://localhost:${port}');
+});
 const form = document.querySelector('form');
 const fullName = document.getElementById("name");
 const email = document.getElementById("email");
 const phone = document.getElementById("phone");
 const message = document.getElementById("message");
 
-function sendEmail(){
-    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value}<br> Message: ${message.value}`;
-
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "nikallen1818@gmail.com",
-        Password : "E645DA40A53AAED7AA3B410DABD37D2360A3",
-        To : 'nikallen1818@gmail.com',
-        From : "nikallen1818@gmail.com",
-        Subject : subject.value,
-        Body : bodyMessage,
-    }).then(
-      message => {
-        if (message == "OK") {
-          Swal.fire({
-            title: "Success!",
-            text: "Message sent successfully!",
-            icon: "success"
-          });
-        }
-      }
-    
-    );
-}
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    sendEmail();
-});
